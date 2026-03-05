5ce75b918961d53b6259a7a8d42ce11d0e287439
0xCatPKG <0xCatPKG@rvvm.dev>
2026-03-05 08:38:17 -0300

    Codegen: add missing `)`

--- src/codegen/xml_scanner.zig
+++ src/codegen/xml_scanner.zig
@@ -185,7 +185,7 @@ pub fn main() !void {
 
     try writer.print(
         \\pub fn destroy(i: *dbuz.types.Proxy, gpa: std.mem.Allocator) void {{
-        \\    const p: *{s} = @alignCast(@ptrCast(@fieldParentPtr("interface", i));
+        \\    const p: *{s} = @alignCast(@ptrCast(@fieldParentPtr("interface", i)));
         \\
         \\    if (!p.properties._inited) return;
         \\    p.properties._mutex.lock();
